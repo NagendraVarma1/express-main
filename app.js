31e8aacf653eab65be02fe17f5e2f63a441d39e0
NagendraVarma1 <nagendravarmal512@gmail.com>
2023-11-27 13:41:59 +0530

    .

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-    console.log('In the second middleware')
+    console.log('In the second middleware');
 })
 
 const server = http.createServer(app);
