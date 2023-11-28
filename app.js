86beb1983862c1991ffd39f813cc6c3625de3339
NagendraVarma1 <nagendravarmal512@gmail.com>
2023-11-28 09:46:03 +0530

    task-3

--- app.js
+++ app.js
@@ -3,21 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./Routes/admin");
+const shopRoutes = require("./Routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
-  );
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Welcome to Express js</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
 });
 
 app.listen(3000);
